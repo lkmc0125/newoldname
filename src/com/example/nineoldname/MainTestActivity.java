c9d2600ce3a83b9893a801484ab4cce959fe392b
Alkmc0125 <Alkmc0125>
2016-06-21 09:46:56 +0800

    nine

---
+++ src/com/example/nineoldname/MainTestActivity.java
@@ -0,0 +1,17 @@
+package com.example.nineoldname;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainTestActivity extends  Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		initView();
+	}
+	public void initView(){
+		
+	}
+
+}
